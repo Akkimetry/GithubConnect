0de5b6c6e874f581bd9dd48023b6998c8c08f8bf
naamannagpaal <hello@namannagpal.me>
2021-10-21 21:20:39 -0400

    Added Main Method

--- src/GitHubConnect.java
+++ src/GitHubConnect.java
@@ -1,3 +1,6 @@
 public class GitHubConnect {
+    public static void main(String[] args) {
+        System.out.println("This change is done by Naman");
+    }
     String newFile = "This is a new file.";
 }
