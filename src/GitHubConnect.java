7e7d74cce656751e3ae6be3dd7a981bbd1f92cde
Akkimetry <aakash.sharma1590@gmail.com>
2021-10-21 21:33:25 -0400

    Added sout statement

--- src/GitHubConnect.java
+++ src/GitHubConnect.java
@@ -1,10 +1,10 @@
 public class GitHubConnect {
     public static void main(String[] args) {
         System.out.println("This change is done by Naman");
+
+        System.out.println("We did it.lol");
     }
     String newFile = "This is a new file.";
 
-    public static void main(String[] args) {
 
-    }
 }
